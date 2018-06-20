6a9650297d47215cde4994ecabb3cc5d5977a383
311235 <36234209+311235@users.noreply.github.com>
2018-06-20 10:57:04 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace U6_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        StreamReader streamReader;
+        string[] numLetters = new string[10];
+        int t;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+       
+            
+
+
+            streamReader = new StreamReader("TestRead.txt");
+            while (!streamReader.EndOfStream)
+            {
+                Console.WriteLine(streamReader.ReadLine());
+            }
+        }
+    }
+}
